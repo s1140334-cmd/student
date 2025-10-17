adfa6567b17ae5187f56494f0f4bbd76f0f42391
s1140334-cmd <s1140334@dmhs.kh.edu.tw>
2025-10-17 16:18:56 +0800

    Add files via upload

---
+++ HW2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    int age = 88;
+    double height = 159.0;
+    std::string name = "Berry";
+    bool is_pass = true;
+    std::cout << "姓名:" << name << " 身高: " << height << std::endl;
+    std::cout << "年齡:" << age<< " 是否及格: " << is_pass << std::endl;
+    return 0;
+}
