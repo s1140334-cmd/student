a5075466148386adf2c5afb6919bb2f2d126b452
s1140334-cmd <s1140334@dmhs.kh.edu.tw>
2025-11-21 16:25:00 +0800

    Add files via upload

---
+++ hw-6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "請輸入你的年齡:請輸入你的年齡:" << endl;    
+    int age;
+    cin >> age;
+    if (age >= 18) {
+        cout <<"18歲成年，可以看 pxxxx網站" << endl;
+    }
+    if (age < 18) {
+        cout <<"未成年，請好好讀書" << endl;
+    }
+    return 0;
+}
